1802cde406f8bee3fcba4f9e4f635b543442240c
carlosepmendes <carlosepmendes@gmail.com>
2015-07-31 12:11:33 +0000

    Added logic for play and pause button

--- app/src/main/java/example/com/soundbox/MainActivity.java
+++ app/src/main/java/example/com/soundbox/MainActivity.java
@@ -1,38 +1,99 @@
 package example.com.soundbox;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener{
+
+    private Button playButton;
+    private Button prevButton;
+    private Button nextButton;
+    private MediaPlayer mediaPlayer;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.game_field);
+
+        text = (TextView) findViewById(R.id.artistName);
+        playButton = (Button) findViewById(R.id.playButton);
+        nextButton = (Button) findViewById(R.id.nextButton);
+        prevButton = (Button) findViewById(R.id.prevButton);
+
+        playButton.setOnClickListener(this);
+        prevButton.setOnClickListener(this);
+        nextButton.setOnClickListener(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void onClick(View v) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (v.getId()){
+            case R.id.playButton:
+                if (mediaPlayer.isPlaying()) {
+                    pauseMusic();
+                }else{
+                    playMusic();
+                }
+                break;
+            case R.id.prevButton:
+                break;
+            case R.id.nextButton:
+                break;
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    public void playMusic(){
+
+        if (mediaPlayer != null){
+            mediaPlayer.start();
+            text.setText("Music playing now...");
+            playButton.setBackgroundDrawable(getResources().getDrawable(android.R.drawable.ic_media_pause));
+        }
+    }
+
+    public void pauseMusic(){
+
+        if (mediaPlayer != null){
+            mediaPlayer.pause();
+            text.setText("Music paused!");
+            playButton.setBackgroundDrawable(getResources().getDrawable(android.R.drawable.ic_media_play));
+        }
+    }
+
+    public void prevMusic(){
+
+        if (mediaPlayer != null){
+            //
+        }
+    }
+
+    public void nextMusic(){
+
+        if (mediaPlayer != null){
+            //
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mediaPlayer != null && mediaPlayer.isPlaying())
+        {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+        super.onDestroy();
     }
 }
